252853f1cfd7b8b318ec448c5996000fcd6c89b0
HirmaanR <rashidabadi.8511@gmail.com>
2025-08-13 07:03:58 +0000

    feat: 🎸 add arch linux config files

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+if vim.loader then
+	vim.loader.enable()
+end
+
+_G.dd = function(...)
+	require("util.debug").dump(...)
+end
+vim.print = _G.dd
+
+require("config.lazy")
+
+if vim.loader then
+	vim.loader.enable()
+end
+
+_G.dd = function(...)
+	require("util.debug").dump(...)
+end
+vim.print = _G.dd
+
+require("config.lazy")
